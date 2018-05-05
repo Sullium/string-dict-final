e149c5d8208c4776ad24851fb49d8391ae257041
Sean <39011381+Sullium@users.noreply.github.com>
2018-05-05 15:11:47 -0400

    Create CompactSearchTree.java

---
+++ CompactSearchTree.java
@@ -0,0 +1,64 @@
+public class CompactSearchTree implements Set{
+    private Node first;
+    private int numElements;
+    
+    public boolean add(Node e){
+    
+    }
+    
+    public boolean addAll(Collection<Node>){
+        
+    }
+    
+    public void clear(){
+    
+    }
+    
+    public boolean contains(Object o){
+    
+    }
+    
+    public boolean containsAll(Collection<Node> c){
+    
+    }
+    
+    public boolean equals(Object o){
+    
+    }
+    
+    public int hashCode(){
+        
+    }
+    
+    public boolean isEmpty(){
+        
+    }
+    
+    public Iterator<Node> iterator(){
+    
+    }
+    
+    public boolean remove(Object o){
+    
+    }
+    
+    public boolean removeAll(Collection<Node> c){
+    
+    }
+    
+    public boolean retainAll(Collection<Node> c){
+    
+    }
+    
+    public int size(){
+    
+    }
+    
+    public Object[] toArray(){
+        
+    }
+    
+    public <T> T[] toArray(T[] a){
+    
+    }
+}
