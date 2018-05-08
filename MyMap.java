ac8298bb96e79cd08e84d58fef1dbe4473ae80fd
Sean <39011381+Sullium@users.noreply.github.com>
2018-05-08 15:42:59 -0400

    Update MyMap.java

--- MyMap.java
+++ MyMap.java
@@ -8,11 +8,11 @@ public final class MyMap<K,V> implements Map<K,V> {
     int size;
     
     public int size() {
-	    return this.size;
+	return this.size;
     }
     
     public Set<Entry<K, V>> entrySet() {
-		return entries;
+	return entries;
     }
 
     public Set<K> keySet() {
