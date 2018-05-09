2be5a3c005bc764dcddec0777c0182ebe14a3f92
Sean <39011381+Sullium@users.noreply.github.com>
2018-05-09 04:14:47 -0400

    Update SearchTreeSet.java

--- SearchTreeSet.java
+++ SearchTreeSet.java
@@ -3,51 +3,52 @@ public class SearchTreeSet implements Set{
     private int numElements;
     
     public boolean add(Node e){
-    
+        return true;
     }
     
-    public boolean addAll(Collection<Node>){
-        
+    public boolean addAll(Collection<Node> c){
+        return true;
     }
     
     public void clear(){
-    
+        
     }
     
     public boolean contains(Object o){
-    
+        return true;
     }
     
     public boolean containsAll(Collection<Node> c){
-    
+        return true;
     }
     
     public boolean equals(Object o){
-    
+        return true;
     }
     
     public int hashCode(){
-        
+        int toReturn;
+        return toReturn;
     }
     
     public boolean isEmpty(){
-        
+        return true;
     }
     
     public Iterator<Node> iterator(){
-    
+        
     }
     
     public boolean remove(Object o){
-    
+        return true;
     }
     
     public boolean removeAll(Collection<Node> c){
-    
+        return true;
     }
     
     public boolean retainAll(Collection<Node> c){
-    
+        return true;
     }
     
     public int size(){
