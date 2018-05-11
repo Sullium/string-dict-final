14ec9f23edfd062582dd2b51df612d3202e75579
Sean <39011381+Sullium@users.noreply.github.com>
2018-05-11 16:44:50 -0400

    Update Node.java

--- Node.java
+++ Node.java
@@ -1,7 +1,7 @@
 public class Node{
     private Character ch;   //  Character stored by this Node
     public  boolean containsElement;    //  Unsure why this is necessary
-    private ArrayList<Character> allNext;
+    private ArrayList<Node> allNext;
     
     public Node(Character c){    //  Constructs a Node containing a character
         this.ch = c;
@@ -19,18 +19,18 @@ public class Node{
     
     public Node getNextNodeAt(int index){
         if(i<numNext){
-            Node toReturn = allNext[i];
+            Node toReturn = allNext.get(i);
             return toReturn;
         }else{
             return null;
         }
     }
     
-    public int getNextNodeIndexWith(char toFind){
-        boolean found = false;
+    public int getNextNodeIndexWith(Character toFind){  //  Returns the index at which parameter character appears
+        boolean found = false;                          //  in this Node's children
         int i=0;        
         while((!found) && (i<numNext)){
-            if(allNext[i].getCharValue() = toFind){
+            if(allNext.get(i).getCharValue().equals(toFind)){
                 found = true;
             }
             i++;
@@ -38,15 +38,15 @@ public class Node{
         if(found)
             return i;
         else
-            return -1;
-    }
+            return -1;  //  Return -1 if not found. Probably safer in general
+    }                   //  than GetNextNodeWith (below) returning null 
     
     public Node getNextNodeWith(char toFind){   //  Finds and returns the Node branching off current one 
         Node toReturn;                          //  which contains parameter character.
         boolean found = false;
         int i=0;
         while((!found) && (i<numNext)){             //  Compare next Nodes until match is found or
-            toReturn = allNext[i];                  //  none are left.
+            toReturn = allNext.get(i);                  //  none are left.
             if(toReturn.getCharValue() == toFind){
                 found = true;
             }
