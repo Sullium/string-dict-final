5c1984681c66cff9080178398e09eb4de639d4eb
Sean <39011381+Sullium@users.noreply.github.com>
2018-05-11 17:24:07 -0400

    Update SearchTreeSet.java

--- SearchTreeSet.java
+++ SearchTreeSet.java
@@ -6,29 +6,29 @@ public class SearchTreeSet implements Set{
     private int size;     // (ie. each node has a pointer to its parent). We had spent much time
                           // trying to work with more strictly minimal Nodes
     public boolean add(Object e){
-	    if(size==0){
-		    first = new Node;
-		}else{
+        if(size==0){
+            first = new Node;
+        }else{
 	    
-		}
-		iterator<n> itr = n.iterator();
-		boolean tf=true;
-		while(it.hasNext) && (tf){  
-			if(e==Node){
-				tf = false;
-			} else{
-				tf =true;
-			}
-		}
-		n=n.next();
-		n=e;
-		size++;
-		return tf;
-	}
+        }
+        iterator<n> itr = n.iterator();
+        boolean tf=true;
+        while(it.hasNext) && (tf){
+            if(e==Node){
+                tf = false;
+            } else{
+                tf =true;
+            }
+        }
+        n=n.next();
+        n=e;
+        size++;
+        return tf;
+    }
     
     public boolean addAll(Collection c){
 	
-		return true;
+        return true;
     }
     
     public void clear(){
@@ -36,59 +36,59 @@ public class SearchTreeSet implements Set{
     }
     
     public boolean contains(Object o){
-		iterator<n> itr = n.iterator();
+        iterator<n> itr = n.iterator();
 	
-		return true;
+        return true;
     }
     
     public boolean containsAll(Collection c){
-		return true;
+        return true;
     }
     
     public boolean equals(Object o){
-		return true;
+        return true;
     }
     
     public int hashCode(){
-		int toReturn;
-		return toReturn;
+        int toReturn;
+        return toReturn;
     }
     
     public boolean isEmpty(){
-		if(size==0){
-			return true;
-		}else {
-			return false;
-		}
-	}
+        if(size==0){
+            return true;
+        }else {
+            return false;
+        }
+    }
     
     public Iterator iterator(){  //  Our confusion on the structure of Nodes made this mostly
-		Iterator itr;            //  impossible to even start
+        Iterator itr;            //  impossible to even start
 	}
     
     public boolean remove(Object o){
-		return true;
+        return true;
     }
     
     public boolean removeAll(Collection c){
-		return true;
+        return true;
     }
     
     public boolean retainAll(Collection c){
-		return true;
+        return true;
     }
     
     public int size(){
-		return numElements;
+        return numElements;
     }
     
     public Object[] toArray(){
-		Object[] o = new Object[];
-		return o;
+        Object[] o = new Object[];
+        return o;
     }
 
     public Object[] toArray(Object[] o){
-		return o;
+        return o;
     }
     
         public <T> T[] toArray(Object a){
